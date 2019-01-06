f581c6e8b08058a27e0722306a556e90c5bdb6c1
SoulP <info@soulp.moe>
2019-01-06 13:40:41 +0900

    first commit

---
+++ src/moe/soulp/Main.java
@@ -0,0 +1,75 @@
+package moe.soulp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import nexon.auth.NexonAuthentication;
+
+public class Main {
+	private static String mabinogi = "mabinogi.exe";
+	private static final String option = "code:1622 ver:321 logip:111.87.32.89 logport:11000 chatip:111.87.32.92 chatport:8002 setting:\"file://data/features.xml=Regular, Japan\" /P:";
+	private static InputStreamReader isr = new InputStreamReader(System.in);
+	private static BufferedReader br = new BufferedReader(isr);
+	public static void main(String[] args) throws IOException {
+		String id = null;
+		String password = null;
+		String otp = "";
+		boolean isOTP = false;
+
+		for(short i = 0; i < args.length; i++) {
+			if(args[i].equalsIgnoreCase("-help") || args[i].equalsIgnoreCase("-h")) {
+				System.out.println("java -jar Mabinogi.jar [options...]");
+				System.out.println();
+				System.out.println("-id ...                   NEXON ID");
+				System.out.println("-password ... | -p ...    Password");
+				System.out.println("-otp [...]                OneTimePassword");
+				System.out.println("-help | -h                HELP");
+				System.exit(0);
+			}
+			if(args[i].equalsIgnoreCase("-id")) {
+				id = args[++i];
+			}
+			if(args[i].equalsIgnoreCase("-password") || args[i].equalsIgnoreCase("-p")) {
+				password = args[++i];
+			}
+			if(args[i].equalsIgnoreCase("-otp")) {
+				isOTP = true;
+				if(args[i+1].matches("^\\d{6}$")) {
+					otp = args[++i];
+				}
+			}
+		}
+
+		if(id == null || id.isEmpty()) {
+			System.out.print("NEXON ID: ");
+			id = br.readLine();
+		}
+
+		if(password == null || password.isEmpty()) {
+			System.out.print("Password: ");
+			char[] pass = System.console().readPassword();
+			password = "";
+			for(short i = 0; i < pass.length; i++) {
+				password += pass[i];
+			}
+		}
+
+		if(isOTP && otp.isEmpty()) {
+			System.out.print("OTP: ");
+			otp = br.readLine();
+		}
+
+		// 認証
+		NexonAuthentication nexonAuth = new NexonAuthentication();
+		String npp = nexonAuth.login(id, password, otp);
+
+		// 起動
+		try {
+			Runtime rt = Runtime.getRuntime();
+			rt.exec(mabinogi + " " + option + npp);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/nexon/dto/SSOInfo.java
@@ -0,0 +1,16 @@
+package nexon.dto;
+
+public class SSOInfo {
+	public boolean ShowFisshingAlert;
+	public boolean LoginSuccess;
+	public boolean IsOtpUser;
+	public boolean IsCaptchaUser;
+	public boolean HasError;
+	public boolean IsBlocked;
+	public boolean RequirePasswordChange;
+	public boolean IsMobileID;
+	public boolean IsSimpleID;
+	public String CaptchaUrl;
+	public boolean LoginFailure;
+	public boolean OtpFailure;
+}
